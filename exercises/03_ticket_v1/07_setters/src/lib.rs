dfc81546663a778331610aa2833ef4366d8c544a
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-10-02 23:38:36 +0300

    Complete chapter 3

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,9 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        Self::check_title_validity(&title);
+        Self::check_description_validity(&description);
+        Self::check_status_validity(&status);
 
         Ticket {
             title,
@@ -44,6 +32,45 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Self::check_title_validity(&new_title);
+        self.title = new_title;
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        Self::check_description_validity(&new_description);
+        self.description = new_description;
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Self::check_status_validity(&new_status);
+        self.status = new_status;
+    }
+
+    fn check_title_validity(new_title: &String) {
+        if new_title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if new_title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    fn check_description_validity(new_description: &String) {
+        if new_description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if new_description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    fn check_status_validity(new_status: &String) {
+        if new_status != "To-Do" && new_status != "In Progress" && new_status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
     }
 }
 
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,75 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::check_product_name_validity(&product_name);
+        Self::check_quantity_validity(&quantity);
+        Self::check_unit_price_validity(&unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        &self.quantity * &self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        Self::check_product_name_validity(&new_product_name);
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        Self::check_quantity_validity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        Self::check_unit_price_validity(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+
+    fn check_product_name_validity(new_product_name: &String) {
+        if new_product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+
+        if new_product_name.len() > 300 {
+            panic!("Product name cannot be longer than 300 bytes");
+        }
+    }
+
+    fn check_quantity_validity(new_quantity: &u32) {
+        if *new_quantity <= 0 {
+            panic!("Quantity should be more than zero");
+        }
+    }
+
+    fn check_unit_price_validity(new_unit_price: &u32) {
+        if *new_unit_price <= 0 {
+            panic!("Unit price should be more than zero");
+        }
+    }
+}
