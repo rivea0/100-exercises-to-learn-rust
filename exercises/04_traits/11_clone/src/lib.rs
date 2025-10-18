ddb9bde0ccc1592532ed98e7a881855839539c97
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-10-18 16:58:29 +0300

    Complete chapter 4

--- exercises/04_traits/11_clone/src/lib.rs
+++ exercises/04_traits/11_clone/src/lib.rs
@@ -2,7 +2,7 @@
 //  to get the code to compile.
 
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+    (ticket.clone(), ticket.summary())
 }
 
 pub struct Ticket {
